ff8744a24cd7f7988e13d5eab03010363c9b26d4
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-05-15 01:24:55 +0200

    feat: meshcomponent

--- Engine/components/MeshComponent.cpp
+++ Engine/components/MeshComponent.cpp
@@ -1,12 +1,16 @@
 #include "MeshComponent.h"
 
-#include <windows.h>
-
-#include "core/GameObject.h"
 #include "core/ServiceLocator.h"
 #include "core/ResourceManager.h"
 
 using namespace mk;
+
+MeshComponent::MeshComponent(std::string vertex, std::string fragment, bool projectedOnScreen)
+	: m_VertexShader{ std::move(vertex) }
+	, m_FragmentShader{ std::move(fragment) }
+	, m_ProjectedOnScreen{ projectedOnScreen }
+{
+}
 
 MeshComponent::~MeshComponent()
 {
@@ -16,4 +20,23 @@ MeshComponent::~MeshComponent()
 void MeshComponent::Start()
 {
 	ServiceLocator::GetRenderer().RegisterRender(this);
+}
+
+void MeshComponent::SetTexture(const std::string& texture)
+{
+}
+
+const std::string& MeshComponent::GetVertexShader() const
+{
+	return m_VertexShader;
+}
+
+const std::string& MeshComponent::GetFragmentShader() const
+{
+	return m_FragmentShader;
+}
+
+bool MeshComponent::IsProjected() const
+{
+	return m_ProjectedOnScreen;
 }
--- Engine/components/MeshComponent.h
+++ Engine/components/MeshComponent.h
@@ -3,7 +3,6 @@
 #include <string_view>
 
 #include "core/IComponent.h"
-#include "core/Transform.h"
 
 namespace mk
 {
@@ -12,7 +11,7 @@ namespace mk
 	class MeshComponent final : public IComponent
 	{
 	public:
-		MeshComponent() = default;
+		MeshComponent(std::string vertex, std::string fragment, bool projectedOnScreen = false);
 		~MeshComponent() override;
 
 		MeshComponent(const MeshComponent& other)				= delete;
@@ -22,6 +21,16 @@ namespace mk
 
 		void Start() override;
 
+		void SetTexture(const std::string& texture);
+
+		const std::string& GetVertexShader() const;
+		const std::string& GetFragmentShader() const;
+		bool IsProjected() const;
+
+
 	private:
+		const std::string m_VertexShader{};
+		const std::string m_FragmentShader{};
+		const bool m_ProjectedOnScreen{ false };
 	};
 }
