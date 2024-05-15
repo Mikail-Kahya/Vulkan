40d0ce69cecac1812543f15bc25ab86dce4dd554
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-05-15 11:39:03 +0200

    chore: remove unused functions

--- Engine/render/IRenderer.h
+++ Engine/render/IRenderer.h
@@ -9,11 +9,9 @@ namespace mk
 		virtual ~IRenderer() = default;
 
 		virtual void Render() const	= 0;
-		virtual void Update() const	= 0;
+		virtual void Update()		= 0;
 		virtual mesh_handle RegisterRender(void* objectPtr)	= 0;
 		virtual void UnregisterRender(mesh_handle handle)	= 0;
-		virtual void SetTexture(mesh_handle handle, const std::string& path)	= 0;
-		virtual void SetMesh(mesh_handle handle, const std::string& path)		= 0;
 
 	protected:
 		IRenderer() = default;
--- Engine/render/NullRenderer.h
+++ Engine/render/NullRenderer.h
@@ -15,10 +15,8 @@ namespace mk
 		NullRenderer& operator=(NullRenderer&& other) noexcept	= delete;
 
 		void Render() const override {}
-		void Update() const override {}
+		void Update() override {}
 		mesh_handle RegisterRender(void*) override { return 0; }
 		void UnregisterRender(mesh_handle) override {}
-		void SetTexture(mesh_handle, const std::string&) override {}
-		void SetMesh(mesh_handle, const std::string&) override {}
 	};
 }
